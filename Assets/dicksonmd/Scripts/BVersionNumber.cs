9c9a20e7d3c7a044e92e98d8d82c25b52e92ddca
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2021-02-21 22:48:59 +0800

    Auto version number label

---
+++ Assets/dicksonmd/Scripts/BVersionNumber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BVersionNumber : MonoBehaviour
+{
+    public Text label;
+    public string template = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (label == null)
+        {
+            label = GetComponent<Text>();
+        }
+        if (template == "")
+        {
+            template = label.text;
+        }
+        label.text = template.Replace("%v", Application.version);
+    }
+}
