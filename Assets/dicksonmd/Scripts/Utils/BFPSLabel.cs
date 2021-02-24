9857c37885515b7bc6d3afa9622e28d500cca825
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2021-02-24 19:38:02 +0800

    Add FPS label; Change level colors; Add Player hitbox sprite

---
+++ Assets/dicksonmd/Scripts/Utils/BFPSLabel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BFPSLabel : MonoBehaviour
+{
+    public Text label;
+    public string template = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (label == null)
+        {
+            label = GetComponent<Text>();
+        }
+        if (template == "")
+        {
+            template = label.text;
+        }
+    }
+
+    void Update()
+    {
+        label.text = (template
+            .Replace("%fps%", "" + (int)(1f / Time.smoothDeltaTime))
+        );
+    }
+}
