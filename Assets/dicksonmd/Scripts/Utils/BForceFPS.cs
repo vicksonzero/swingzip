2f7ef015e5df3af0df3acb83b030c289a5bda2f1
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2021-03-01 23:57:44 +0800

    Add forced FPS for android

---
+++ Assets/dicksonmd/Scripts/Utils/BForceFPS.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BForceFPS : MonoBehaviour
+{
+    public int targetFrameRate = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
