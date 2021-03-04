ad95abeae589d64a6615b0101c35bb19f3eb9195
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2021-03-04 20:16:10 +0800

    Add breathing title screen

--- Assets/dicksonmd/Scripts/BDeliveryObjective.cs
+++ Assets/dicksonmd/Scripts/BDeliveryObjective.cs
@@ -152,7 +152,7 @@ public class BDeliveryObjective : MonoBehaviour
                 TimeSpan timeSpan = TimeSpan.FromSeconds(record);
                 var playCount = PlayerPrefs.GetInt("Record.playCount", 0);
                 int recordScorePerMinute = PlayerPrefs.GetInt("Record.scorePerMinute", 0);
-                int recordLanding = PlayerPrefs.GetInt("Record.landing", 0);
+                int recordLanding = PlayerPrefs.GetInt("Record.landing", 10000);
                 recordsLabel.text = ("" +
                     playCount + "\n" +
                     String.Format("{0:D2}:{1:D2}:{2:D2}.{3:D3}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds) + "\n" +
---
+++ Assets/dicksonmd/Scripts/Utils/BBreathingImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BBreathingImage : MonoBehaviour
+{
+    [Tooltip("can be null")]
+    public Image image;
+    public float speed = 1;
+    public float min = 0.6f;
+    public float max = 1;
+
+    private float alphaProgress = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (image == null) image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alphaProgress += speed * Time.deltaTime;
+        var color = image.color;
+        color.a = min + (Mathf.Sin(alphaProgress) + 1) / 2 * (max - min);
+        image.color = color;
+    }
+}
